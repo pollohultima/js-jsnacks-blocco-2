01692ba9934c05f6bae2fc3eb2fabf0d3eab487c
pollohultima <matteo.polonio@gmail.com>
2021-10-25 12:20:42 +0200

    part 1 done

---
+++ snack_2/main.js
@@ -0,0 +1,29 @@
+/* Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti e falli subiti.
+Nome sarà l’unica proprietà da compilare, mentre le altre saranno tutte settate a 0. */
+
+const teams = [
+    {
+        teamName: 'Inter',
+        points: 0,
+        faults: 0
+    },
+
+    {
+        teamName: 'Juventus',
+        points: 0,
+        faults: 0
+    },
+
+    {
+        teamName: 'Milan',
+        points: 0,
+        faults: 0
+    },
+
+    {
+        teamName: 'Lazio',
+        points: 0,
+        faults: 0
+    }
+]
